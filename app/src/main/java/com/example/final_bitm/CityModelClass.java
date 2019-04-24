a4efef6d71a8210f1d82a64e281a9383eb5e25a8
Noyon-Dey <noyondey8@gmail.com>
2019-04-24 20:44:14 +0600

    Seventh commit

---
+++ app/src/main/java/com/example/final_bitm/CityModelClass.java
@@ -0,0 +1,34 @@
+package com.example.final_bitm;
+
+
+public class CityModelClass {
+
+    int cityId;
+    String cityName;
+
+    public CityModelClass(int cityId, String cityName) {
+        this.cityId = cityId;
+        this.cityName = cityName;
+    }
+
+    public CityModelClass(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
+
